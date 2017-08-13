ba9d7c6b2719c8eaa05d23559248ab8977f12423
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-13 05:27:31 +0300

    init

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import SimpleForm from './SimpleForm';
+
+export default class Home extends Component {
+  render() {
+    return <SimpleForm />;
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form';
+
+const rootReducer = combineReducers({
+  form: formReducer,
+  routing: routerReducer,
+});
+
+export default rootReducer;
